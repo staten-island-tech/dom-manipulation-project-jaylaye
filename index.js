a5e2ea82caff19ad399c3f970028bcdbd1ea6b19
jaylaye <yejayla69@gmail.com>
2023-10-20 09:27:14 -0400

    dommy mwommy

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+    button: document.getElementById("btn"),
+    text: document.querySelector("#text"),
+    point: document.querySelector(".point"),
+
+};
+console.log(DOMSelectors.point);
