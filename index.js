ec6c311888933af311b489435434b17b6e97a956
jaylaye <yejayla69@gmail.com>
2023-10-25 09:29:58 -0400

    dommy mommy

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const DOMSelectors = {
     button: document.getElementById("btn"),
     text: document.querySelector("#text"),
+    box: document.getElementById("big-black-box"),
     point: document.querySelector(".point"),
 
 };
+function backgroundAndText(background, text){
+    background.style.backgroundColor = "red";
+    text.innerHTML = "This is now a big red box";
+    text.style.fontSize = "40px";
+}
 console.log(DOMSelectors.point);
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+DOMSelectors.button.addEventListener("click", function(){
+    backgroundAndText(DOMSelectors.box, DOMSelectors.text)
+});
+
+function changeLi(){
+    let pointIndex = 1;
+    DOMSelectors.points.forEach((point) => {
+point.addEventListener("click", function (){
+    point.textContent = 'Hello I am point ${pointIndex}';
+    pointIndex++;
+});
+
+    });
+}
+changeLi();
