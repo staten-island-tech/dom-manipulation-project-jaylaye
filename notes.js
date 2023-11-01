934c9e52194d001ec0b55c179e0e358c81401f41
jaylaye <yejayla69@gmail.com>
2023-11-01 09:29:38 -0400

    notezz

---
+++ notes.js
@@ -0,0 +1,25 @@
+ /* for (i = 14; i > 7; i--){
+    console.log(i);
+}
+money = 100;
+while(money > 0){
+    cosole.log(money);
+    money--;
+}
+
+function linearSearch(num, arr){
+    let found;
+    for(let i=0; 1 < arr.length; i++){
+        if(arr{i} === num){
+            found = arr{i};
+            break;
+        } else {
+            console.log("still looking")
+        }
+    }
+    console.log(found);
+    return found;
+}
+
+linearSearch(5, numsArr);
+*/
